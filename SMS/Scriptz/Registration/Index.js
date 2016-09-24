6b3f524bfd01c869a166ae843537d7713920b338
gopi123 <gopikshnan@gmail.com>
2016-09-24 20:04:56 +0530

    Added dupemail sending code

--- SMS/Scriptz/Registration/Index.js
+++ SMS/Scriptz/Registration/Index.js
@@ -8,18 +8,18 @@ $(function () {
         var currMonth = $("#ddlMonth").val();
 
         table = $(".dTable").dataTable({
-            
+
             "bProcessing": true,//to show processing word
             "autoWidth": false,//to adjust width  
             "scrollX": true,
-            "destroy":true,
+            "destroy": true,
             "aaSorting": [[10, 'desc']],
             "width": "20%",
             "ajax": {
                 "url": url,
                 "method": "GET",
                 "dataType": "json",
-                "data": function(d){
+                "data": function (d) {
                     d.finYear = currFinYear;
                     d.month = currMonth;
                 }
@@ -42,12 +42,12 @@ $(function () {
 
             //Defining checkbox in columns
             "aoColumnDefs": [
-               
+
                 {
                     "targets": [8],
                     "bSortable": false,
                     "render": function (data, type, row) {
-                        if (row.NextDueAmount != 0) {                          
+                        if (row.NextDueAmount != 0) {
                             return data + ',' + row.NextDueAmount
                         }
                         else {
@@ -76,6 +76,10 @@ $(function () {
                                        '<a class="btn btn-xs btn-info receipt pull-left"  data-id=' + data + ' >' +
                                        '<i class="fa fa-rupee "></i> Receipt</a>' +
                                    '</div>' +
+                                 '<div class=" pull-left">' +
+                                       '<a class="btn btn-xs btn-info dupEmail pull-left"  data-id=' + data + ' >' +
+                                       '<i class="fa  fa-envelope-o "></i> Email</a>' +
+                                   '</div>' +
                                '</div>'
                         }
                         else {
@@ -83,11 +87,15 @@ $(function () {
                                    '<div class="pull-left">' +
                                        '<a class="btn btn-xs btn-info editData pull-left"  data-id=' + data + ' >' +
                                        '<i class="fa fa-pencil"></i> Edit</a>' +
-                                   '</div>' +                                   
+                                   '</div>' +
+                                   '<div class=" pull-left">' +
+                                       '<a class="btn btn-xs btn-info dupEmail pull-left"  data-id=' + data + ' >' +
+                                       '<i class="fa  fa-envelope-o "></i> Email</a>' +
+                                   '</div>' +
                                '</div>'
                         }
-                       
-                              
+
+
                     }
                 }
 
@@ -105,6 +113,7 @@ $(function () {
                     url = url.replace('param1_placeholder', id);
                     window.location.href = url;
                 });
+
             }
         });
 
@@ -114,15 +123,54 @@ $(function () {
 
     $(document).on("change", "#ddlFinYearList", function () {
         $('.dTable').dataTable().fnDestroy();
-        GetDataTable();    
-        
+        GetDataTable();
+
     });
 
     $(document).on("change", "#ddlMonth", function () {
         $('.dTable').dataTable().fnDestroy();
         GetDataTable();
     });
-   
+
+    $(document).on('click', '.dupEmail', function () {
+
+        var $emailButton = $(this);
+        var box = bootbox.confirm("Are you sure you want to resend Registration Email once again", function (result) {
+            if (result) {
+                box.modal('hide');
+                $.blockUI({ message: null });
+
+                var id = $emailButton.data('id');
+                var href = $("#frmIndex").data("dupemail-url");
+
+                $.ajax({
+                    type: "GET",
+                    url: href,
+                    data: { RegId: id },
+                    datatype: "json",
+                    success: function (data) {
+                        $.unblockUI();
+                        if (data == "success") {
+                            bootbox.alert("Successfully send the email details.Please ask the customer to check <b>spam/junk</b> folder");
+
+                        }
+                        else {
+                            bootbox.alert("Error.. Cannot send email");
+                        }
+                        //toastr.success("Successfully saved the details.")
+
+
+                    },
+                    error: function (err) {
+                        toastr.error("Error:" + this.message)
+                    }
+                });
+            };
+        });
+        return false;
+    });
+
+
 
 
 
