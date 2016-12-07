3bf5f24050a90b37f32afe171e4d903696ddf847
gopi123 <gopikshnan@gmail.com>
2016-12-07 11:14:47 +0530

    Added swal

--- SMS/Scriptz/CourseFullEditApproval/Add.js
+++ SMS/Scriptz/CourseFullEditApproval/Add.js
@@ -58,18 +58,73 @@ $(function () {
     });
 
     var ShowApprovalAlert = function () {
+        //initial approval or reject popup
         swal({
-            title: "Are you sure?",
-            text: "You will not be able to recover this imaginary file!",
-            type: "warning",
+            title: "Approve or Reject?",
+            text: "Please click below to approve or reject",
+            type: "info",
             showCancelButton: true,
-            confirmButtonColor: "#DD6B55",
-            confirmButtonText: "Yes, delete it!",
-            closeOnConfirm: false
+            confirmButtonColor: "#55dd7a",
+            confirmButtonText: "Approve",
+            cancelButtonText:"Reject",
+            closeOnConfirm: false,
+            closeOnCancel: false
         },
-        function () {
-            swal("Deleted!", "Your imaginary file has been deleted.", "success");
+        function (isConfirm) {
+            if (isConfirm) {
+                //on clicking approve button
+                swal({
+                    title: "Are you sure?",
+                    text: "You will not be able to undo the change!",
+                    type: "warning",
+                    showCancelButton: true,
+                    confirmButtonColor: "#55dd7a",
+                    confirmButtonText: "Yes, approve it!",
+                    closeOnConfirm: true
+                },
+                function () {
+                    setTimeout(function () {
+                        ajaxUpdate();
+                    }, 250);
+                    
+                });
+            }
+            else {
+                //on clicking delete button
+                swal({
+                    title: "Rejected Reason!",
+                    text: "Enter reason for rejection:",
+                    type: "input",
+                    showCancelButton: true,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "Reject it!",
+                    closeOnConfirm: false,
+                    animation: "slide-from-top",
+                    inputPlaceholder: "Enter reason"
+                },
+                function (inputValue) {
+                    if (inputValue === false) return false;
+
+                    if (inputValue === "") {
+                        swal.showInputError("Please enter reason!");
+                        return false
+                    }
+
+                    
+                });
+            }
+            
         });
     }
+
+    
+
+    var ajaxUpdate = function () 
+    {
+        
+        
+        alert("processing.....");
+    }
+
 
 })
